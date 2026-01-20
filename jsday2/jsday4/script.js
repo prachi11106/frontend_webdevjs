203a2597c089b132255a9192dd62f2a82f2b1c42
prachi kaushik <kaushikprachi400@gmail.com>
2026-01-20 09:53:25 +0530

    2nd commit

---
+++ jsday2/jsday4/script.js
@@ -0,0 +1,27 @@
+//let numbers = [1,2,3,4,5];
+//let sliced = numbers.slice(1,4);
+//console.log(sliced); // Output: [2, 3, 4]
+
+//let number = [10,20,30,40,50];
+//let hasEven = number.some(x => x % 2 === 0);
+//console.log(hasEven);
+
+//let number1 = [2,5,6,7,8];
+//let allEven = number1.every(x => x%2 == 0);
+//console.log(allEven);
+ 
+ //let fruits1 = ["apple", "banana"];
+ //let fruits2 = ["cherry", "orange"];
+ //let combined = fruits1.concat(fruits2);
+ //console.log(combined);
+
+ //splice example
+ //let arr = [1,2,3,4,5];
+ //arr.splice(2,1,'a','b');
+ //console.log(arr); // Output: [1, 2, 'a', 'b', 4, 5]
+
+ //sir example
+ let fruits = ["apple", "banana", "cherry"];
+ fruits.splice(1, 1, "orange", "grape");
+ console.log(fruits); // Output: ["apple", "orange", "grape", "cherry"]
+
