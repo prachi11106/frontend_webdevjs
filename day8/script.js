d836a616ce9fe79f48536a002b2d553c53a44360
prachi kaushik <kaushikprachi400@gmail.com>
2026-01-30 15:49:28 +0530

    day8

---
+++ day8/script.js
@@ -0,0 +1,17 @@
+const heading = document.getElementById("heading");
+console.log(heading);
+
+heading.style.color = "blue";
+heading.style.backgroundColor = "yellow";
+
+console.log(heading.textContent);
+heading.textContent = "Hello from JavaScript!";
+
+const subHeading = document.getElementsByClassName("sub-heading");
+console.log(subHeading);    
+
+for(let i = 0; i < subHeading.length; i++) {
+    subHeading[i].style.fontSize = "24px";
+    subHeading[i].textContent = "Sub-heading modified by JavaScript!";
+    subHeading[1].style.color = "green";
+}
