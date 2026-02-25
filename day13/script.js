7aa7f8434e253d95a701e3c0de2d5acfbdf08f4d
prachi kaushik <kaushikprachi400@gmail.com>
2026-02-25 10:48:45 +0530

    day13

---
+++ day13/script.js
@@ -0,0 +1,11 @@
+console.log("my")
+setTimeout(() => {
+    console.log("kaushik")
+}, 5000)
+setTimeout(() => {
+    console.log("prachi")
+}, 3000)
+setTimeout(() => {
+    console.log("is")
+}, 2000)
+console.log("name")
